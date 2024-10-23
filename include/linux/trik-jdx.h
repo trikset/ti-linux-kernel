0c259d9a743a1fda44de7bde7aff2e64c0087eeb
Chehade Daniel (Шехаде Даниэль) <71555323+danielsheh02@users.noreply.github.com>
2024-10-23 18:31:58 +0300

    Add US-HCSR-04 module, add nodes for LED in DT

---
+++ include/linux/trik-jdx.h
@@ -0,0 +1,10 @@
+#ifndef __TRIK_JDX_H__
+#define __TRIK_JDX_H__
+#include <linux/platform_data/gpio-davinci.h>
+
+struct trik_jdx_platform_data {
+	int gpio_d1a;
+	int gpio_d1b;
+};
+
+#endif  /* __JDX_H__ */
